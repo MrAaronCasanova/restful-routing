30ccdb0e5fac5eae62c24d9cacabe1ddf7de9e35
MrAaronCasanova <1boris231@gmail.com>
2018-01-16 07:49:58 -0800

    implementing CRUD functionality, about to work on the delete route

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
+var methodOverride = require('method-override');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var express = require('express');
+var app = express();
 
 // APP CONFIG
-var app = express();
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(methodOverride('_method'));
 app.use(bodyParser.urlencoded({ extended: true }));
+
 mongoose.connect('mongodb://localhost/restful_blog_app', { useMongoClient: true });
 
 // MONGOOSE/MODEL CONFIG
@@ -24,12 +27,64 @@ app.get('/', function (req, res) {
   res.redirect('/blogs');
 });
 
+// INDEX ROUTE
 app.get('/blogs', function (req, res) {
   Blog.find({}, function (err, blogs) {
     if (err) {
       console.log('ERROR');
     } else {
       res.render('index', { blogs: blogs });
+    }
+  });
+});
+
+// NEW ROUTE
+app.get('/blogs/new', function (req, res) {
+  res.render('new');
+});
+
+// CREATE ROUTE
+app.post('/blogs', function (req, res) {
+  // Create blog
+  Blog.create(req.body.blog, function (err, newBlog) {
+    if (err) {
+      res.render('new');
+    } else {
+      // Redirect to Index
+      res.redirect('/blogs');
+    }
+  });
+});
+
+// SHOW ROUTE
+app.get('/blogs/:id', function (req, res) {
+  Blog.findById(req.params.id, function (err, foundBlog) {
+    if (err) {
+      res.redirect('/blogs');
+    } else {
+      res.render('show', { blog: foundBlog });
+    }
+  });
+});
+
+// EDIT ROUTE
+app.get('/blogs/:id/edit', function (req, res) {
+  Blog.findById(req.params.id, function (err, foundBlog) {
+    if (err) {
+      res.redirect('/blogs');
+    } else {
+      res.render('edit', { blog: foundBlog });
+    }
+  });
+});
+
+// UPDATE ROUTE
+app.put('/blogs/:id', function (req, res) {
+  Blog.findByIdAndUpdate(req.params.id, req.body.blog, function (err, updatedBlog) {
+    if (err) {
+      res.redirect('/blogs');
+    } else {
+      res.redirect('/blogs/' + req.params.id);
     }
   });
 });
